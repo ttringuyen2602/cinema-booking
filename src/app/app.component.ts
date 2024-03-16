89483a6d4c07eefeae646bc5833daa80477fc643
Lynx.Nguyen <ttringuyen2602@gmail.com>
2024-03-16 22:32:59 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component } from "@angular/core";
+import { RouterLink, RouterOutlet } from "@angular/router";
+import { interval } from "rxjs";
+import { CarouselState } from "@src/app/models/CarouselState";
+import { NzCarouselModule } from "ng-zorro-antd/carousel";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: "app-root",
+    standalone: true,
+    imports: [RouterOutlet, RouterLink, NzCarouselModule],
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.scss"
 })
-export class AppComponent {
-  title = 'cinema-booking-ui';
+export class AppComponent implements AfterViewInit {
+    title = "cinema-booking-ui";
+    carousel = new CarouselState();
+    array = [1, 2, 3, 4];
+    constructor() {
+    }
+
+    ngAfterViewInit(): void {
+        interval(5000).subscribe(() => {
+            this.nextCarousel();
+        });
+    }
+
+    nextCarousel(): void {
+        const currentTime = Date.now();
+        if (currentTime - this.carousel.lastClickTime >= this.carousel.delayTime) {
+            return;
+        }
+        this.carousel.id++;
+        if (this.carousel.id >= this.carousel.img.length) {
+            this.carousel.id = 0;
+        }
+    }
+
+    previousCarousel(): void {
+        const currentTime = Date.now();
+        if (currentTime - this.carousel.lastClickTime >= this.carousel.delayTime) {
+            return;
+        }
+        this.carousel.id--;
+        if (this.carousel.id < 0) {
+            this.carousel.id = this.carousel.img.length - 1;
+        }
+    }
+
 }
---
+++ src/app/models/CarouselState.ts
@@ -0,0 +1,14 @@
+export class CarouselState {
+    id: number = 0;
+    img: string[] = [
+        "gotg3-2.jpg",
+        "lat-mat-6.jpg",
+        "combo-u22-digital.jpg",
+        "combo-dd.jpg",
+        "quet-vn-pay.jpg"
+    ];
+    carouselPath: string = "assets/images/carousel/";
+    carouselSrc: string = "gotg3-2.jpg";
+    delayTime: number = 550;
+    lastClickTime: number = 0;
+}
