e7b0d9084353801ad797db641edcc80e57c9e395
Kantee <tunguyen3692@gmail.com>
2024-03-17 11:31:03 +0700

    Config structure

---
+++ src/app/pages/public/detail-movie/detail-movie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-movie',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-movie.component.html',
+  styleUrl: './detail-movie.component.scss'
+})
+export class DetailMovieComponent {
+
+}
